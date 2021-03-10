96f0aacec1d82246c7de248a782e477d2ff3a5ee
Edward Huang <ejhuang.2015@gmail.com>
2021-03-10 11:36:53 -0500

    Updated ordering and added lib files

--- index.js
+++ index.js
@@ -12,78 +12,78 @@ function mainMenu() {
         pageSize: 10,
         message: "What would you like to do?",
         choices: [
+            new inquirer.Separator(), // Add functions
             "Add a Department",
             "Add a Role",
             "Add an Employee",
-            new inquirer.Separator(),
+            new inquirer.Separator(), // View functions
             "View All Departments",
             "View All Roles",
             "View All Employees",
-            new inquirer.Separator(),
+            "View All Employees by Manager",
+            "View a Department's Total Budget",
+            new inquirer.Separator(), // Remove functions
             "Remove a Department",
             "Remove a Role",
             "Remove an Employee",
-            new inquirer.Separator(),
+            new inquirer.Separator(), // Update functions
             "Update an Employee's Role",
             "Update an Employee's Manager",
-            "View All Employees by Manager",
-            "View a Department's Total Budget",
-            new inquirer.Separator(),
-            "Exit",
-            new inquirer.Separator(),
+            new inquirer.Separator(), // Exit
+            "Exit"
         ]
     }).then((res) => {
         switch (res.startSelection) {
             case 'Add a Department':
-            //   artistSearch();
-              break;
+                //   artistSearch();
+                break;
             case 'Add a Role':
-            //   multiSearch();
-              break;
+                //   multiSearch();
+                break;
             case 'Add an Employee':
-            //   rangeSearch();
-              break;
+                //   rangeSearch();
+                break;
             case 'View All Departments':
-            //   songSearch();
-              break;
+                //   songSearch();
+                break;
             case 'View All Roles':
-            //   songAndAlbumSearch();
-              break;
+                //   songAndAlbumSearch();
+                break;
             case 'View All Employees':
-            //   songAndAlbumSearch();
-              break;
-            case 'Remove a Department':
-            //   songSearch();
-              break;
-            case 'Remove a Role':
-            //   songAndAlbumSearch();
-              break;
-            case 'Remove an Employee':
-            //   songAndAlbumSearch();
-              break;
-            case "Update an Employee's Role":
-            //   songSearch();
-              break;
-            case "Update an Employee's Manager":
-            //   songAndAlbumSearch();
-              break;
+                //   songAndAlbumSearch();
+                break;
             case 'View All Employees by Manager':
-            //   songAndAlbumSearch();
-              break;
+                //   songAndAlbumSearch();
+                break;
             case "View a Department's Total Budget":
-            //   songAndAlbumSearch();
-              break;
+                //   songAndAlbumSearch();
+                break;
+            case 'Remove a Department':
+                //   songSearch();
+                break;
+            case 'Remove a Role':
+                //   songAndAlbumSearch();
+                break;
+            case 'Remove an Employee':
+                //   songAndAlbumSearch();
+                break;
+            case "Update an Employee's Role":
+                //   songSearch();
+                break;
+            case "Update an Employee's Manager":
+                //   songAndAlbumSearch();
+                break;
             case "Exit":
-              console.log("Thanks for using the Employee Tracker. Goodbye.")
-              break;
+                console.log("Thanks for using the Employee Tracker. Goodbye.")
+                break;
             default:
-              console.log(`Uh oh, you broke me! Tell my creator how I died...`);
+                console.log(`Uh oh, you broke me! Tell my creator how I died...`);
         }
     })
 };
 
 // Fun ASCII art
-function splashArt(){
+function splashArt() {
     console.log(`
   ____________________  
 ((                    ))
